423465014fe304b5456644aabd6345f0bddb5fee
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-05 11:45:25 +0545

    feat: problem with async opetaion in action

--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,6 +1,13 @@
 
-export const productListing = () => {
-    let data = "hello"
+export const productListing = async () => {
+    let data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    // calling the api
+    // problem with this is: jaba samma data aauxa teti bela samma program agadi gai sakya hunxa
+    //The problem with this code is that productListing is an asynchronous function that returns a Promise that resolves to a plain object representing an action. However, Redux expects action creators to return plain objects directly, not Promises.
+
+    data = await data.json()
+    console.log(data)
+
     console.warn("action called", data)
     return {
         type: "PRODUCT_LIST",

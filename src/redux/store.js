7211e35006519db84f649ed417a1dd4fafd3d914
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-05 12:16:17 +0545

    feat: configure saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,12 +7,13 @@ import createSagaMiddleware from "redux-saga"
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+// here, we are using the createStore function from redux to create a store and pass in the rootReducer and the sagaMiddleware as arguments.
 
 // const store = configureStore({
 //     reducer: rootReducer,
 // middleware: () => [sagaMiddleware]
 // })
 
-sagaMiddleware.run(productSaga)
+sagaMiddleware.run(productSaga)    // run the saga
 
 export default store
